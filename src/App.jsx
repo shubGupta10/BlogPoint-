9c765d60fe49652705f9526323aad8dcf25a28d9
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-25 18:28:30 +0530

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
+  
 
   useEffect(() => {
     authService.getCurrentUser()
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
 
 
+
 import AddPost from "./pages/AddPost";
 import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
