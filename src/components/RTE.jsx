05ec0a340922d91157e909e295a3e76e05287dac
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-05 21:58:02 +0530

    Bugs FIxes

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,6 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
+        apiKey={config.tinyMCEKey}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
