d45e2ae670d8d6ef81b4397eb2e365142356f465
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-11 11:40:56 +0530

    New Commit

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -6,7 +6,7 @@ const conf = {
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     apikey: String(import.meta.env.VITE_APPWRITE_API_KEY),
 }
-// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
+
 
 export default conf
 
