f9fd2d268b0a3bb7799e61a93e16b9f3e0245253
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-11 11:31:06 +0530

    New Commit

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -3,7 +3,7 @@ import {Editor } from '@tinymce/tinymce-react';
 import {Controller } from 'react-hook-form';
 
 
-export default function RTE({name, control, label, defaultValue =""}) {
+export default function RTE({name, control, label, defaultValue ="",  apiKey}) {
   return (
     <div className='w-full'> 
     {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey={config.tinyMCEKey}
+        apiKey={apiKey}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
