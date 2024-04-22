98f059bce13ba0df3e79b814ff9a46ca3f903752
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-22 11:43:39 +0530

    Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div style={{fontSize: '50px', fontWeight: 'bold'}}>BlogPoint</div>
+    <div style={{fontSize: '30px', fontWeight: 'bold'}}>BlogPoint</div>
   )
 }
 
